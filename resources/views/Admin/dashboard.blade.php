45b05334a3ac8297da645156c7609b8146bfff92
akbar tri wicaksono <akbartriwicaksono46@gmail.com>
2025-03-26 21:49:18 +0800

    set template

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('Layouts.Base')
+
+@section('content')
+    <h1>cieee full senyum bulan ini</h1>
+@endsection
+
+@section('scripts')
+@endsection
---
+++ resources/views/Layouts/Sidebar.blade.php
@@ -0,0 +1,35 @@
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+            <div class="app-brand demo">
+                <a href="index.html" class="app-brand-link">
+                    <span class="app-brand-logo demo">
+                        <img src="{{ asset('assets/assets/ahp.png') }}" alt="Logo" class="img-fluid" width="50"
+                            height="50">
+                    </span>
+                    <span class="text-start app-brand-text fw-bold ms-2 ">
+                        <small>AHP</small><br>
+                        <small>Marching Band</small><br>
+                        <small>SMAN 1 PALASA</small>
+                    </span>
+                </a>
+
+                <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                    <i class="bx bx-chevron-left bx-sm align-middle"></i>
+                </a>
+            </div>
+
+            <div class="menu-inner-shadow"></div>
+
+            <ul class="menu-inner py-1">
+                <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
+                    <a href="/dashboard" class="menu-link">
+                        <i class="menu-icon fa-solid fa-house"></i>
+                        <div data-i18n="Analytics">Dashboard</div>
+                    </a>
+                </li>
+
+
+            </ul>
+        </aside>
+        <!-- / Menu -->
