d6dd46ef81b84c99dce725b50991d408ccdff306
divta-suryawan <gitdivta@gmail.com>
2025-03-26 21:37:17 +0800

    create controller and route for criteria anda criteria values

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\CMS\CriteriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// route api
+Route::prefix('v1')->group(function () {
+    // Routes criteria
+    Route::prefix('criteria')->controller(CriteriaController::class)->group(function () {
+        Route::get('/', 'getAllData');
+    });
+
+    // Routes criteria-values
+    Route::prefix('criteria-values')->controller(CriteriaController::class)->group(function () {
+        Route::get('/', 'getAllData');
+        Route::post('/create', 'createData');
+        Route::get('/get/{id}', 'getDataById');
+        Route::post('/update/{id}', 'updateData');
+        Route::delete('/delete/{id}', 'deleteData');
+    });
 });
